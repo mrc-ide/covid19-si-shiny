3b6a82c1995ade4f560104774c0d99166e99b98b
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-07-29 12:16:46 +0100

    Lots of code duplicated but getting what I want

--- app.R
+++ app.R
@@ -7,17 +7,33 @@ models_grid <- function(ids) {
     column(4, plotOutput(ids[3]))
   )
 }
-ui <- navbarPage(
+ui <- fluidPage(
   titlePanel("Robust estimates of the infectiousness profile and serial interval distributions of SARS-CoV-2"),
   fluidRow(
     column(
       12, selectInput("view", "Choose outputs to view", choices = c("across models", "across datasets"))
     )
   ),
-  models_grid(c("s3", "s3mix", "s3recall")),
-  models_grid(c("s3mixrecall", "s4", "s4mix")),
-  models_grid(c("s4recall", "s4mixrecall", "s3s4mix"))
-
+  tabsetPanel(
+    tabPanel(
+      "NF",
+      models_grid(c("s3", "s3mix", "s3recall")),
+      models_grid(c("s3mixrecall", "s4", "s4mix")),
+      models_grid(c("s4recall", "s4mixrecall", "s3s4mix"))
+    ),
+    tabPanel(
+      "skew normal",
+        models_grid(c("sns3", "sns3mix", "sns3recall")),
+      models_grid(c("sns3mixrecall", "sns4", "sns4mix")),
+      models_grid(c("sns4recall", "sns4mixrecall", "sns3s4mix"))
+    ),
+    tabPanel(
+      "gamma",
+      models_grid(c("gs3", "gs3mix", "gs3recall")),
+      models_grid(c("gs3mixrecall", "gs4", "gs4mix")),
+      models_grid(c("gs4recall", "gs4mixrecall", "gs3s4mix"))
+    )
+  )
 )
 server <- function(input, output, session) {
   output$s3 <- renderPlot({
@@ -51,6 +67,74 @@ server <- function(input, output, session) {
   })
 
   output$s3s4mix <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+  ## Skew normal
+  output$sns3 <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$sns3mix <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$sns3recall <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+  output$sns4 <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$sns4mix <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$sns4recall <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+  output$sns4mixrecall <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$sns3mixrecall <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$sns3s4mix <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$gs3 <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$gs3mix <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$gs3recall <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+  output$gs4 <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$gs4mix <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$gs4recall <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+  output$gs4mixrecall <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$gs3mixrecall <- renderPlot({
+    ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+  })
+
+  output$gs3s4mix <- renderPlot({
     ggplot(mtcars, aes(mpg, cyl)) + geom_point()
   })
 }
